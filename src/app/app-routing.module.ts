7247be6639700759a468647cdb915703831445b6
Sergey Serdyuk <fxduke@gmail.com>
2020-02-29 10:08:06 +0200

    draft of the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { RepoListComponent } from './components/search/repo-list/repo-list.component';
+import { RepoDetailsComponent } from './components/search/repo-details/repo-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'repositories/:username', component: RepoListComponent },
+  { path: 'repository/:id', component: RepoDetailsComponent },
+  { path: '',
+    redirectTo: '/search',
+    pathMatch: 'full'
+  },
+  { path: '**', component: SearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { RepoListComponent } from './components/search/repo-list/repo-list.component';
+import { RepoDetailsComponent } from './components/search/repo-details/repo-details.component';
+import { SharedModule } from './modules/shared/shared.module';
+import { DataService } from './services/data.service';
+import { ModelService } from './services/model.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    RepoListComponent,
+    RepoDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    DataService,
+    ModelService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
